3793c564a7fc6dd5b201a2479f0bcd21ca265c87
glottotopia <sebastian.nordhoff@glottotopia.de>
2021-10-08 14:47:31 +0200

    copy from Overleaf

---
+++ names.sed
@@ -0,0 +1,169 @@
+sed -i 's/Alan Prince/\name{Alan}{Prince}/g' *
+sed -i 's/Albert Gallatin/\name{Albert}{Gallatin}/g' *
+sed -i 's/Albert Sechehaye/\name{Albert}{Sechehaye}/g' *
+sed -i 's/Alden Mason/\name{Alden}{Mason}/g' *
+sed -i 's/Alexei Šaxmatov//\name{Alexei}{Šaxmatov}/g' *
+sed -i 's/Alfred Kroeber/\name{Alfred}{Kroeber}/g' *
+sed -i 's/Alice Bloomfield/\name{Alice}{Bloomfield}/g' *
+sed -i 's/André Martinet/\name{André}{Martinet}/g' *
+sed -i 's/Antoine Meillet/\name{Antoine}{Meillet}/g' *
+sed -i 's/Antoni Dufriche-Desgenettes/\name{Antoni}{Dufriche-Desgenettes}/g' *
+sed -i 's/Antony Flew/\name{Antony}{Flew}/g' *
+sed -i 's/Archibald Hill/\name{Archibald}{Hill}/g' *
+sed -i 's/Arthur Lloyd/\name{Arthur}{Lloyd}/g' *
+sed -i 's/August Leskien/\name{August}{Leskien}/g' *
+sed -i 's/Benjamin Franklin/\name{Benjamin}{Franklin}/g' *
+sed -i 's/Benjamin Whorf/\name{Benjamin}{Whorf}/g' *
+sed -i 's/Bernard Bloch/\name{Bernard}{Bloch}/g' *
+sed -i 's/Bert Vaux/\name{Bert}{Vaux}/g' *
+sed -i 's/Bill Darden/\name{Bill}{Darden}/g' *
+sed -i 's/Bill Poser/\name{Bill}{Poser}/g' *
+sed -i 's/Bob Ladd/\name{Bob}{Ladd}/g' *
+sed -i 's/Bronislaw Malinowski/\name{Bronislaw}{Malinowski}/g' *
+sed -i 's/Carl Voegelin/\name{Carl}{Voegelin}/g' *
+sed -i 's/Carsten Høeg/\name{Carsten}{Høeg}/g' *
+sed -i 's/Changliang Qu/\name{Changliang}{Qu}/g' *
+sed -i 's/Charles Hockett/\name{Charles}{Hockett}/g' *
+sed -i 's/Claude Levi-Strauss/\name{Claude}{Levi-Strauss}/g' *
+sed -i 's/Colin Cherry/\name{Colin}{Cherry}/g' *
+sed -i 's/Daniel Jones/\name{Daniel}{Jones}/g' *
+sed -i 's/David Abercrombie/\name{David}{Abercrombie}/g' *
+sed -i 's/David Hunt/\name{David}{Hunt}/g' *
+sed -i 's/David Stampe/\name{David}{Stampe}/g' *
+sed -i 's/Dennis Klatt/\name{Dennis}{Klatt}/g' *
+sed -i 's/Eberhardt Zwirner/\name{Eberhardt}{Zwirner}/g' *
+sed -i 's/Eddy Roulet/\name{Eddy}{Roulet}/g' *
+sed -i 's/Edgar Sturtevant/\name{Edgar}{Sturtevant}/g' *
+sed -i 's/Eduard Prokosch/\name{Eduard}{Prokosch}/g' *
+sed -i 's/Eduard Sievers/\name{Eduard}{Sievers}/g' *
+sed -i 's/Edward Salisbury/\name{Edward}{Salisbury}/g' *
+sed -i 's/Edward Sapir/\name{Edward}{Sapir}/g' *
+sed -i 's/Elan Dresher/\name{Elan}{Dresher}/g' *
+sed -i 's/Eli Fischer-Jørgensen/\name{Eli}{Fischer-Jørgensen}/g' *
+sed -i 's/Elizabeth Uldall/\name{Elizabeth}{Uldall}/g' *
+sed -i 's/Émile Benveniste/\name{Émile}{Benveniste}/g' *
+sed -i 's/Eugene Nida/\name{Eugene}{Nida}/g' *
+sed -i 's/Florence Delson Sapir/\name{Florence Delson}{Sapir}/g' *
+sed -i 's/Frances Ingemann/\name{Frances}{Ingemann}/g' *
+sed -i 's/Frances Johnson/\name{Frances}{Johnson}/g' *
+sed -i 's/Francis Whitfield/\name{Francis}{Whitfield}/g' *
+sed -i 's/Frans Gregersen/\name{Frans}{Gregersen}/g' *
+sed -i 's/Franz Boas/\name{Franz}{Boas}/g' *
+sed -i 's/Fred Householder/\name{Fred}{Householder}/g' *
+sed -i 's/Freeman Twaddell/\name{Freeman}{Twaddell}/g' *
+sed -i 's/Fritz Newmeyer/\name{Fritz}{Newmeyer}/g' *
+sed -i 's/Georg Curtius/\name{Georg}{Curtius}/g' *
+sed -i 's/George Bolling/\name{George}{Bolling}/g' *
+sed -i 's/George Hunt/\name{George}{Hunt}/g' *
+sed -i 's/George Trager/\name{George}{Trager}/g' *
+sed -i 's/Gunnar Fant/\name{Gunnar}{Fant}/g' *
+sed -i 's/Gunver Skytte/\name{Gunver}{Skytte}/g' *
+sed -i 's/Hans Basbøll/\name{Hans}{Basbøll}/g' *
+sed -i 's/Hans Jørgen Uldall/\name{Hans Jørgen}{Uldall}/g' *
+sed -i 's/Hélène Coustenoble/\name{Hélène}{Coustenoble}/g' *
+sed -i 's/Henry Higgins/\name{Henry}{Higgins}/g' *
+sed -i 's/Henryk Ułaszyn/\name{Henryk}{Ułaszyn}/g' *
+sed -i 's/Henry Lee Smith/\name{Henry Lee}{Smith}/g' *
+sed -i 's/Henry Sweet/\name{Henry}{Sweet}/g' *
+sed -i 's/Hermann Osthoff/\name{Hermann}{Osthoff}/g' *
+sed -i 's/Hermann Paul/\name{Hermann}{Paul}/g' *
+sed -i 's/Holger Pedersen/\name{Holger}{Pedersen}/g' *
+sed -i 's/Jakob Wackernagel/\name{Jakob}{Wackernagel}/g' *
+sed -i 's/Janet Pierrehumbert/\name{Janet}{Pierrehumbert}/g' *
+sed -i 's/Jan Niecisław Baudouin de Courtenay/\name{Jan Niecisław Baudouin de}{Courtenay}/g' *
+sed -i 's/Jean McClenaghan/\name{Jean}{McClenaghan}/g' *
+sed -i 's/Jeanne Allard/\name{Jeanne}{Allard}/g' *
+sed -i 's/Jeffrey Watamull/\name{Jeffrey}{Watamull}/g' *
+sed -i 's/Joan Bybee/\name{Joan}{Bybee}/g' *
+sed -i 's/Joe Salmons/\name{Joe}{Salmons}/g' *
+sed -i 's/Johann Herder/\name{Johann}{Herder}/g' *
+sed -i 's/John Goldsmith/\name{John}{Goldsmith}/g' *
+sed -i 's/John Halle/\name{John}{Halle}/g' *
+sed -i 's/John Lotz/\name{John}{Lotz}/g' *
+sed -i 's/John Ohala/\name{John}{Ohala}/g' *
+sed -i 's/John Rupert Firth/\name{John Rupert}{Firth}/g' *
+sed -i 's/John Wesley Powell/\name{John Wesley}{Powell}/g' *
+sed -i 's/Jørgen Jørgensen/\name{Jørgen}{Jørgensen}/g' *
+sed -i 's/Jørgen Rischel/\name{Jørgen}{Rischel}/g' *
+sed -i 's/Joseph Greenberg/\name{Joseph}{Greenberg}/g' *
+sed -i 's/Jost Winteler/\name{Jost}{Winteler}/g' *
+sed -i 's/Karen Mikkelsen-Sørensen/\name{Karen}{Mikkelsen-Sørensen}/g' *
+sed -i 's/Karl Brugmann/\name{Karl}{Brugmann}/g' *
+sed -i 's/Karl Verner/\name{Karl}{Verner}/g' *
+sed -i 's/Kenneth Howell/\name{Kenneth}{Howell}/g' *
+sed -i 's/Kenneth Pike/\name{Kenneth}{Pike}/g' *
+sed -i 's/Kristian Sandfeld/\name{Kristian}{Sandfeld}/g' *
+sed -i 's/Alfred Kroeber/\name{Alfred}{Kroeber}/g' *
+sed -i 's/Larry Horn/\name{Larry}{Horn}/g' *
+sed -i 's/Leonard Bloomfield/\name{Leonard}{Bloomfield}/g' *
+sed -i 's/Lev Vladimiovič Ščerba/\name{Lev Vladimiovič}{Ščerba}/g' *
+sed -i 's/Arthur Lloyd James/\name{Arthur Lloyd}{James}/g' *
+sed -i 's/Louis Hammerich/\name{Louis}{Hammerich}/g' *
+sed -i 's/Louis Havet/\name{Louis}{Havet}/g' *
+sed -i 's/Louis Trolle Hjelmslev/\name{Louis Trolle}{Hjelmslev}/g' *
+sed -i 's/Lucy (Homikanis) Hunt/\name{Lucy (Homikanis)}{Hunt}/g' *
+sed -i 's/Marguérite Durand/\name{Marguérite}{Durand}/g' *
+sed -i 's/Marie-Eugénie Faesch/\name{Marie-Eugénie}{Faesch}/g' *
+sed -i 's/Mark Liberman/\name{Mark}{Liberman}/g' *
+sed -i 's/Martin Joos/\name{Martin}{Joos}/g' *
+sed -i 's/Mary Beckman/\name{Mary}{Beckman}/g' *
+sed -i 's/Mary Rosamond Haas/\name{Mary Rosamond}{Haas}/g' *
+sed -i 's/Maurice Grammont/\name{Maurice}{Grammont}/g' *
+sed -i 's/Max Muller/\name{Max}{Muller}/g' *
+sed -i 's/Michel Lejeune/\name{Michel}{Lejeune}/g' *
+sed -i 's/Mikołaj Kruszewski/\name{Mikołaj}{Kruszewski}/g' *
+sed -i 's/Morris Halle/\name{Morris}{Halle}/g' *
+sed -i 's/Morris Swadesh/\name{Morris}{Swadesh}/g' *
+sed -i 's/Murray Emeneau/\name{Murray}{Emeneau}/g' *
+sed -i 's/Nelson Goodman/\name{Nelson}{Goodman}/g' *
+sed -i 's/Nigel Vincent/\name{Nigel}{Vincent}/g' *
+sed -i 's/Nikolaj Sergeevič Trubetzkoy/\name{Nikolaj Sergeevič}{Trubetzkoy}/g' *
+sed -i 's/Nina Grønnum/\name{Nina}{Grønnum}/g' *
+sed -i 's/Noah Webster/\name{Noah}{Webster}/g' *
+sed -i 's/Noam Chomsky/\name{Noam}{Chomsky}/g' *
+sed -i 's/Noémi Mallet/\name{Noémi}{Mallet}/g' *
+sed -i 's/Otto Jespersen/\name{Otto}{Jespersen}/g' *
+sed -i 's/Outi Bat-El/\name{Outi}{Bat-El}/g' *
+sed -i 's/Patricia Donegan/\name{Patricia}{Donegan}/g' *
+sed -i 's/Paul Diderichsen/\name{Paul}{Diderichsen}/g' *
+sed -i 's/Paul Kiparsky/\name{Paul}{Kiparsky}/g' *
+sed -i 's/Paul Passy/\name{Paul}{Passy}/g' *
+sed -i 's/Paul Smolensky/\name{Paul}{Smolensky}/g' *
+sed -i 's/Paul Verrier/\name{Paul}{Verrier}/g' *
+sed -i 's/Peter Duponceau/\name{Peter}{Duponceau}/g' *
+sed -i 's/Pierre Fouché/\name{Pierre}{Fouché}/g' *
+sed -i 's/Poul Andersen/\name{Poul}{Andersen}/g' *
+sed -i 's/Rasmus Rask/\name{Rasmus}{Rask}/g' *
+sed -i 's/René Descartes/\name{René}{Descartes}/g' *
+sed -i 's/Richard McKeon/\name{Richard}{McKeon}/g' *
+sed -i 's/Robert Lees/\name{Robert}{Lees}/g' *
+sed -i 's/Robert Stockwell/\name{Robert}{Stockwell}/g' *
+sed -i 's/Roger Lass/\name{Roger}{Lass}/g' *
+sed -i 's/Roman Jakobson/\name{Roman}{Jakobson}/g' *
+sed -i 's/Ronald Feldstein/\name{Ronald}{Feldstein}/g' *
+sed -i 's/Rulon Wells/\name{Rulon}{Wells}/g' *
+sed -i 's/Sergej Karcevskij/\name{Sergej}{Karcevskij}/g' *
+sed -i 's/Tim Halle/\name{Tim}{Halle}/g' *
+sed -i 's/Tony Tillohash/\name{Tony}{Tillohash}/g' *
+sed -i 's/Uriel Weinreich/\name{Uriel}{Weinreich}/g' *
+sed -i 's/Vicki Fromkin/\name{Vicki}{Fromkin}/g' *
+sed -i 's/Viggo Brøndal/\name{Viggo}{Brøndal}/g' *
+sed -i 's/Vilém Mathesius/\name{Vilém}{Mathesius}/g' *
+sed -i 's/Vilhelm Thomsen/\name{Vilhelm}{Thomsen}/g' *
+sed -i 's/Walter Dyk/\name{Walter}{Dyk}/g' *
+sed -i 's/William Chomsky/\name{William}{Chomsky}/g' *
+sed -i 's/William Dwight Whitney/\name{William Dwight}{Whitney}/g' *
+sed -i 's/William Labov/\name{William}{Labov}/g' *
+sed -i 's/Will Leben/\name{Will}{Leben}/g' *
+sed -i 's/Elsie Winifred Owen/\name{Elsie Winifred}{Owen}/g' *
+sed -i 's/Yevgeny Polivanov/\name{Yevgeny}{Polivanov}/g' *
+sed -i 's/Zellig Harris/\name{Zellig}{Harris}/g' *
+
+sed -i 's/Alfred P. Weiss/\name{Alfred P.}{Weiss}/g' *
+sed -i 's/Alva L. Davis/\name{Alva L.}{Davis}/g' *
+sed -i 's/Francis A. Wood/\name{Francis A.}{Wood}/g' *
+sed -i 's/James D. McCawley/\name{James D.}{McCawley}/g' *
+sed -i 's/Stephen O. Murray/\name{Stephen O.}{Murray}/g' *
+sed -i 's/Stephen R. Anderson/\name{Stephen R.}{Anderson}/g' *
+sed -i 's/Thomas G. Masaryk/\name{Thomas G.}{Masaryk}/g' *
+sed -i 's/William M. Austin/\name{William M.}{Austin}/g' *
---
+++ names.sh
@@ -0,0 +1,171 @@
+for f in chapters/*tex; do
+sed -i 's/Alan Prince/\\name{Alan}{Prince}/g' $f
+sed -i 's/Albert Gallatin/\\name{Albert}{Gallatin}/g' $f
+sed -i 's/Albert Sechehaye/\\name{Albert}{Sechehaye}/g' $f
+sed -i 's/Alden Mason/\\name{Alden}{Mason}/g' $f
+sed -i 's/Alexei Šaxmatov//\\name{Alexei}{Šaxmatov}/g' $f
+sed -i 's/Alfred Kroeber/\\name{Alfred}{Kroeber}/g' $f
+sed -i 's/Alice Bloomfield/\\name{Alice}{Bloomfield}/g' $f
+sed -i 's/André Martinet/\\name{André}{Martinet}/g' $f
+sed -i 's/Antoine Meillet/\\name{Antoine}{Meillet}/g' $f
+sed -i 's/Antoni Dufriche-Desgenettes/\\name{Antoni}{Dufriche-Desgenettes}/g' $f
+sed -i 's/Antony Flew/\\name{Antony}{Flew}/g' $f
+sed -i 's/Archibald Hill/\\name{Archibald}{Hill}/g' $f
+sed -i 's/Arthur Lloyd/\\name{Arthur}{Lloyd}/g' $f
+sed -i 's/August Leskien/\\name{August}{Leskien}/g' $f
+sed -i 's/Benjamin Franklin/\\name{Benjamin}{Franklin}/g' $f
+sed -i 's/Benjamin Whorf/\\name{Benjamin}{Whorf}/g' $f
+sed -i 's/Bernard Bloch/\\name{Bernard}{Bloch}/g' $f
+sed -i 's/Bert Vaux/\\name{Bert}{Vaux}/g' $f
+sed -i 's/Bill Darden/\\name{Bill}{Darden}/g' $f
+sed -i 's/Bill Poser/\\name{Bill}{Poser}/g' $f
+sed -i 's/Bob Ladd/\\name{Bob}{Ladd}/g' $f
+sed -i 's/Bronislaw Malinowski/\\name{Bronislaw}{Malinowski}/g' $f
+sed -i 's/Carl Voegelin/\\name{Carl}{Voegelin}/g' $f
+sed -i 's/Carsten Høeg/\\name{Carsten}{Høeg}/g' $f
+sed -i 's/Changliang Qu/\\name{Changliang}{Qu}/g' $f
+sed -i 's/Charles Hockett/\\name{Charles}{Hockett}/g' $f
+sed -i 's/Claude Levi-Strauss/\\name{Claude}{Levi-Strauss}/g' $f
+sed -i 's/Colin Cherry/\\name{Colin}{Cherry}/g' $f
+sed -i 's/Daniel Jones/\\name{Daniel}{Jones}/g' $f
+sed -i 's/David Abercrombie/\\name{David}{Abercrombie}/g' $f
+sed -i 's/David Hunt/\\name{David}{Hunt}/g' $f
+sed -i 's/David Stampe/\\name{David}{Stampe}/g' $f
+sed -i 's/Dennis Klatt/\\name{Dennis}{Klatt}/g' $f
+sed -i 's/Eberhardt Zwirner/\\name{Eberhardt}{Zwirner}/g' $f
+sed -i 's/Eddy Roulet/\\name{Eddy}{Roulet}/g' $f
+sed -i 's/Edgar Sturtevant/\\name{Edgar}{Sturtevant}/g' $f
+sed -i 's/Eduard Prokosch/\\name{Eduard}{Prokosch}/g' $f
+sed -i 's/Eduard Sievers/\\name{Eduard}{Sievers}/g' $f
+sed -i 's/Edward Salisbury/\\name{Edward}{Salisbury}/g' $f
+sed -i 's/Edward Sapir/\\name{Edward}{Sapir}/g' $f
+sed -i 's/Elan Dresher/\\name{Elan}{Dresher}/g' $f
+sed -i 's/Eli Fischer-Jørgensen/\\name{Eli}{Fischer-Jørgensen}/g' $f
+sed -i 's/Elizabeth Uldall/\\name{Elizabeth}{Uldall}/g' $f
+sed -i 's/Émile Benveniste/\\name{Émile}{Benveniste}/g' $f
+sed -i 's/Eugene Nida/\\name{Eugene}{Nida}/g' $f
+sed -i 's/Florence Delson Sapir/\\name{Florence Delson}{Sapir}/g' $f
+sed -i 's/Frances Ingemann/\\name{Frances}{Ingemann}/g' $f
+sed -i 's/Frances Johnson/\\name{Frances}{Johnson}/g' $f
+sed -i 's/Francis Whitfield/\\name{Francis}{Whitfield}/g' $f
+sed -i 's/Frans Gregersen/\\name{Frans}{Gregersen}/g' $f
+sed -i 's/Franz Boas/\\name{Franz}{Boas}/g' $f
+sed -i 's/Fred Householder/\\name{Fred}{Householder}/g' $f
+sed -i 's/Freeman Twaddell/\\name{Freeman}{Twaddell}/g' $f
+sed -i 's/Fritz Newmeyer/\\name{Fritz}{Newmeyer}/g' $f
+sed -i 's/Georg Curtius/\\name{Georg}{Curtius}/g' $f
+sed -i 's/George Bolling/\\name{George}{Bolling}/g' $f
+sed -i 's/George Hunt/\\name{George}{Hunt}/g' $f
+sed -i 's/George Trager/\\name{George}{Trager}/g' $f
+sed -i 's/Gunnar Fant/\\name{Gunnar}{Fant}/g' $f
+sed -i 's/Gunver Skytte/\\name{Gunver}{Skytte}/g' $f
+sed -i 's/Hans Basbøll/\\name{Hans}{Basbøll}/g' $f
+sed -i 's/Hans Jørgen Uldall/\\name{Hans Jørgen}{Uldall}/g' $f
+sed -i 's/Hélène Coustenoble/\\name{Hélène}{Coustenoble}/g' $f
+sed -i 's/Henry Higgins/\\name{Henry}{Higgins}/g' $f
+sed -i 's/Henryk Ułaszyn/\\name{Henryk}{Ułaszyn}/g' $f
+sed -i 's/Henry Lee Smith/\\name{Henry Lee}{Smith}/g' $f
+sed -i 's/Henry Sweet/\\name{Henry}{Sweet}/g' $f
+sed -i 's/Hermann Osthoff/\\name{Hermann}{Osthoff}/g' $f
+sed -i 's/Hermann Paul/\\name{Hermann}{Paul}/g' $f
+sed -i 's/Holger Pedersen/\\name{Holger}{Pedersen}/g' $f
+sed -i 's/Jakob Wackernagel/\\name{Jakob}{Wackernagel}/g' $f
+sed -i 's/Janet Pierrehumbert/\\name{Janet}{Pierrehumbert}/g' $f
+sed -i 's/Jan Niecisław Baudouin de Courtenay/\\name{Jan Niecisław Baudouin de}{Courtenay}/g' $f
+sed -i 's/Jean McClenaghan/\\name{Jean}{McClenaghan}/g' $f
+sed -i 's/Jeanne Allard/\\name{Jeanne}{Allard}/g' $f
+sed -i 's/Jeffrey Watamull/\\name{Jeffrey}{Watamull}/g' $f
+sed -i 's/Joan Bybee/\\name{Joan}{Bybee}/g' $f
+sed -i 's/Joe Salmons/\\name{Joe}{Salmons}/g' $f
+sed -i 's/Johann Herder/\\name{Johann}{Herder}/g' $f
+sed -i 's/John Goldsmith/\\name{John}{Goldsmith}/g' $f
+sed -i 's/John Halle/\\name{John}{Halle}/g' $f
+sed -i 's/John Lotz/\\name{John}{Lotz}/g' $f
+sed -i 's/John Ohala/\\name{John}{Ohala}/g' $f
+sed -i 's/John Rupert Firth/\\name{John Rupert}{Firth}/g' $f
+sed -i 's/John Wesley Powell/\\name{John Wesley}{Powell}/g' $f
+sed -i 's/Jørgen Jørgensen/\\name{Jørgen}{Jørgensen}/g' $f
+sed -i 's/Jørgen Rischel/\\name{Jørgen}{Rischel}/g' $f
+sed -i 's/Joseph Greenberg/\\name{Joseph}{Greenberg}/g' $f
+sed -i 's/Jost Winteler/\\name{Jost}{Winteler}/g' $f
+sed -i 's/Karen Mikkelsen-Sørensen/\\name{Karen}{Mikkelsen-Sørensen}/g' $f
+sed -i 's/Karl Brugmann/\\name{Karl}{Brugmann}/g' $f
+sed -i 's/Karl Verner/\\name{Karl}{Verner}/g' $f
+sed -i 's/Kenneth Howell/\\name{Kenneth}{Howell}/g' $f
+sed -i 's/Kenneth Pike/\\name{Kenneth}{Pike}/g' $f
+sed -i 's/Kristian Sandfeld/\\name{Kristian}{Sandfeld}/g' $f
+sed -i 's/Alfred Kroeber/\\name{Alfred}{Kroeber}/g' $f
+sed -i 's/Larry Horn/\\name{Larry}{Horn}/g' $f
+sed -i 's/Leonard Bloomfield/\\name{Leonard}{Bloomfield}/g' $f
+sed -i 's/Lev Vladimiovič Ščerba/\\name{Lev Vladimiovič}{Ščerba}/g' $f
+sed -i 's/Arthur Lloyd James/\\name{Arthur Lloyd}{James}/g' $f
+sed -i 's/Louis Hammerich/\\name{Louis}{Hammerich}/g' $f
+sed -i 's/Louis Havet/\\name{Louis}{Havet}/g' $f
+sed -i 's/Louis Trolle Hjelmslev/\\name{Louis Trolle}{Hjelmslev}/g' $f
+sed -i 's/Lucy (Homikanis) Hunt/\\name{Lucy (Homikanis)}{Hunt}/g' $f
+sed -i 's/Marguérite Durand/\\name{Marguérite}{Durand}/g' $f
+sed -i 's/Marie-Eugénie Faesch/\\name{Marie-Eugénie}{Faesch}/g' $f
+sed -i 's/Mark Liberman/\\name{Mark}{Liberman}/g' $f
+sed -i 's/Martin Joos/\\name{Martin}{Joos}/g' $f
+sed -i 's/Mary Beckman/\\name{Mary}{Beckman}/g' $f
+sed -i 's/Mary Rosamond Haas/\\name{Mary Rosamond}{Haas}/g' $f
+sed -i 's/Maurice Grammont/\\name{Maurice}{Grammont}/g' $f
+sed -i 's/Max Muller/\\name{Max}{Muller}/g' $f
+sed -i 's/Michel Lejeune/\\name{Michel}{Lejeune}/g' $f
+sed -i 's/Mikołaj Kruszewski/\\name{Mikołaj}{Kruszewski}/g' $f
+sed -i 's/Morris Halle/\\name{Morris}{Halle}/g' $f
+sed -i 's/Morris Swadesh/\\name{Morris}{Swadesh}/g' $f
+sed -i 's/Murray Emeneau/\\name{Murray}{Emeneau}/g' $f
+sed -i 's/Nelson Goodman/\\name{Nelson}{Goodman}/g' $f
+sed -i 's/Nigel Vincent/\\name{Nigel}{Vincent}/g' $f
+sed -i 's/Nikolaj Sergeevič Trubetzkoy/\\name{Nikolaj Sergeevič}{Trubetzkoy}/g' $f
+sed -i 's/Nina Grønnum/\\name{Nina}{Grønnum}/g' $f
+sed -i 's/Noah Webster/\\name{Noah}{Webster}/g' $f
+sed -i 's/Noam Chomsky/\\name{Noam}{Chomsky}/g' $f
+sed -i 's/Noémi Mallet/\\name{Noémi}{Mallet}/g' $f
+sed -i 's/Otto Jespersen/\\name{Otto}{Jespersen}/g' $f
+sed -i 's/Outi Bat-El/\\name{Outi}{Bat-El}/g' $f
+sed -i 's/Patricia Donegan/\\name{Patricia}{Donegan}/g' $f
+sed -i 's/Paul Diderichsen/\\name{Paul}{Diderichsen}/g' $f
+sed -i 's/Paul Kiparsky/\\name{Paul}{Kiparsky}/g' $f
+sed -i 's/Paul Passy/\\name{Paul}{Passy}/g' $f
+sed -i 's/Paul Smolensky/\\name{Paul}{Smolensky}/g' $f
+sed -i 's/Paul Verrier/\\name{Paul}{Verrier}/g' $f
+sed -i 's/Peter Duponceau/\\name{Peter}{Duponceau}/g' $f
+sed -i 's/Pierre Fouché/\\name{Pierre}{Fouché}/g' $f
+sed -i 's/Poul Andersen/\\name{Poul}{Andersen}/g' $f
+sed -i 's/Rasmus Rask/\\name{Rasmus}{Rask}/g' $f
+sed -i 's/René Descartes/\\name{René}{Descartes}/g' $f
+sed -i 's/Richard McKeon/\\name{Richard}{McKeon}/g' $f
+sed -i 's/Robert Lees/\\name{Robert}{Lees}/g' $f
+sed -i 's/Robert Stockwell/\\name{Robert}{Stockwell}/g' $f
+sed -i 's/Roger Lass/\\name{Roger}{Lass}/g' $f
+sed -i 's/Roman Jakobson/\\name{Roman}{Jakobson}/g' $f
+sed -i 's/Ronald Feldstein/\\name{Ronald}{Feldstein}/g' $f
+sed -i 's/Rulon Wells/\\name{Rulon}{Wells}/g' $f
+sed -i 's/Sergej Karcevskij/\\name{Sergej}{Karcevskij}/g' $f
+sed -i 's/Tim Halle/\\name{Tim}{Halle}/g' $f
+sed -i 's/Tony Tillohash/\\name{Tony}{Tillohash}/g' $f
+sed -i 's/Uriel Weinreich/\\name{Uriel}{Weinreich}/g' $f
+sed -i 's/Vicki Fromkin/\\name{Vicki}{Fromkin}/g' $f
+sed -i 's/Viggo Brøndal/\\name{Viggo}{Brøndal}/g' $f
+sed -i 's/Vilém Mathesius/\\name{Vilém}{Mathesius}/g' $f
+sed -i 's/Vilhelm Thomsen/\\name{Vilhelm}{Thomsen}/g' $f
+sed -i 's/Walter Dyk/\\name{Walter}{Dyk}/g' $f
+sed -i 's/William Chomsky/\\name{William}{Chomsky}/g' $f
+sed -i 's/William Dwight Whitney/\\name{William Dwight}{Whitney}/g' $f
+sed -i 's/William Labov/\\name{William}{Labov}/g' $f
+sed -i 's/Will Leben/\\name{Will}{Leben}/g' $f
+sed -i 's/Elsie Winifred Owen/\\name{Elsie Winifred}{Owen}/g' $f
+sed -i 's/Yevgeny Polivanov/\\name{Yevgeny}{Polivanov}/g' $f
+sed -i 's/Zellig Harris/\\name{Zellig}{Harris}/g' $f
+
+sed -i 's/Alfred P. Weiss/\\name{Alfred P.}{Weiss}/g' $f
+sed -i 's/Alva L. Davis/\\name{Alva L.}{Davis}/g' $f
+sed -i 's/Francis A. Wood/\\name{Francis A.}{Wood}/g' $f
+sed -i 's/James D. McCawley/\\name{James D.}{McCawley}/g' $f
+sed -i 's/Stephen O. Murray/\\name{Stephen O.}{Murray}/g' $f
+sed -i 's/Stephen R. Anderson/\\name{Stephen R.}{Anderson}/g' $f
+sed -i 's/Thomas G. Masaryk/\\name{Thomas G.}{Masaryk}/g' $f
+sed -i 's/William M. Austin/\\name{William M.}{Austin}/g' $f
+done
